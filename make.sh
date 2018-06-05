0073e51e85effc60d76b7d27ae5949e2a4f4164c
Pierre Accorsi <pierre.accorsi@shopify.com>
2018-06-05 15:29:36 -0400

    Revamp install script

--- make.sh
+++ make.sh
@@ -4,34 +4,30 @@ path="$(pwd)"
 
 # Brew
 brew update
-brew install neovim
-brew install zsh zsh-completions
-brew install fzf
-brew install wget
-brew install antigen 
-brew install ag
+brew bundle
 
 # ZSH
-ln -svf $path/zsh/zshenv ~/.zshenv
-ln -svf $path/zsh/zshrc ~/.zshrc
-ln -svf $path/zsh/aliases.zsh ~/.aliases
-ln -svf $path/zsh/exports.zsh ~/.exports
-ln -svf $path/zsh/functions.zsh ~/.functions
+ln -svf "$path/zsh/zshenv" ~/.zshenv
+ln -svf "$path/zsh/zshrc" ~/.zshrc
+ln -svf "$path/zsh/aliases.zsh" ~/.aliases
+ln -svf "$path/zsh/exports.zsh" ~/.exports
+ln -svf "$path/zsh/functions.zsh" ~/.functions
 touch ~/.hushlogin # Avoid "Last Login: xyz"
 
 # GIT
-ln -svf $path/git/gitconfig ~/.gitconfig
-ln -svf $path/git/gitignore ~/.gitignore
+ln -svf "$path/git/gitconfig" ~/.gitconfig
+ln -svf "$path/git/gitignore" ~/.gitignore
 
 # VIM
-ln -svf $path/vim/nvim/config ~/.config
-ln -svf $path/vim/ideavimrc ~/.ideavimrc
+mkdir -p ~/.config/nvim
+ln -svf "$path/vim/nvim/config/nvim" ~/.config/nvim
+ln -svf "$path/vim/ideavimrc" ~/.ideavimrc
 
 # Postgres
-ln -svf $path/psqlrc ~/.psqlrc
+ln -svf "$path/psqlrc" ~/.psqlrc
 
 # Additional scripts
-ln -svf $path/startup.py ~/startup.py
+ln -svf "$path/startup.py" ~/startup.py
 
 # Antigen
 antibody bundle <"$path/bundles.txt" > ~/.zsh_plugins
